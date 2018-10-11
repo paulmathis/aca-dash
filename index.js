1a9a26cd5f8c31100d925463e3e0340b58d19a61
Paul Mathis <paul.mathis@gmail.com>
2018-10-11 13:15:49 -0500

    added find, findLast, head functions

--- index.js
+++ index.js
@@ -43,13 +43,24 @@ function filter(array, iteratee) {
 //     passing in the item from the current loop
 //fnc will return true or false, if true return the item
 //after looping, return null
-function find(theArray, fnc) {}
+function find(theArray, fnc) {
+  for (let i = 0; i < theArray.length; i++) {
+    if (fnc(theArray[i])) {
+      return theArray[i];
+    }
+  }
+  return null;
+}
 
 //return the last item in theArray
-function findLast(theArray) {}
+function findLast(theArray) {
+  return theArray[theArray.length - 1];
+}
 
 //return the first element of the array
-function head(theArray) {}
+function head(theArray) {
+  return theArray[0];
+}
 
 //create a new array
 //loop theArray in reverse order
